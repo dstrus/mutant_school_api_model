cfa8720090a8b35e5b74e52af2b9c63288d9e43c
Dave Strus <dave@getfretless.com>
2016-02-08 15:11:04 -0500

    Test Mutant#find.

--- spec/mutant_school_api_model/mutant_spec.rb
+++ spec/mutant_school_api_model/mutant_spec.rb
@@ -18,5 +18,38 @@ end
 
 describe MutantSchoolAPIModel::Mutant, '#find' do
   it 'should retrieve the mutant that was just created' do
+    attributes = {
+      mutant_name: 'Wolverine',
+      real_name: 'Logan',
+      power: 'SNIKT',
+      eligibility_begins_at: '1976-06-11',
+      eligibility_ends_at: '2050-05-03',
+      may_advise_beginning_at: '1990-09-25'
+    }
+    wolverine = Mutant.new attributes
+    wolverine.save
+
+    actual = Mutant.find(wolverine.id)
+    _(actual.to_h).must_equal(wolverine.to_h)
+  end
+
+  it 'should return false if there is no such record' do
+    attributes = {
+      mutant_name: 'Wolverine',
+      real_name: 'Logan',
+      power: 'SNIKT',
+      eligibility_begins_at: '1976-06-11',
+      eligibility_ends_at: '2050-05-03',
+      may_advise_beginning_at: '1990-09-25'
+    }
+    wolverine = Mutant.new attributes
+    wolverine.save
+    id = wolverine.id
+
+    wolverine.destroy
+    actual = Mutant.find(id)
+    _(actual).must_equal false
+    # _(actual).must_be_instance_of(Array)
+    # _(actual.empty?).must_equal true
   end
 end
