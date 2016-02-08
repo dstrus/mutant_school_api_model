2bcb7dc1c1ed5a4faeb7d28c41ba73adc315372a
Dave Strus <dave@getfretless.com>
2016-02-08 11:01:51 -0500

    Extract base url to a class method, and make Mutant.find return a hash.

--- lib/mutant_school_api_model/mutant.rb
+++ lib/mutant_school_api_model/mutant.rb
@@ -1,16 +1,22 @@
 module MutantSchoolAPIModel
   class Mutant
 
+    def self.base_url
+      'https://mutant-school.herokuapp.com/api/v1/mutants'
+    end
+
     # Get all mutants from the backend
     # mutants = Mutant.all
     def self.all
-      response = HTTP.get('https://mutant-school.herokuapp.com/api/v1/mutants')
+      response = HTTP.get(self.base_url)
       JSON.parse response.to_s
     end
 
     # Get a single mutant from the backend
     # m = Mutant.find(3)
     def self.find(id)
+      response = HTTP.get(self.base_url + "/#{id}")
+      JSON.parse response.to_s
     end
 
     # Create or update a mutant in the backend
