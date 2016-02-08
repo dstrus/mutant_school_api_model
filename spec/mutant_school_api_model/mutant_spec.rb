4a2e2ddf59453d19e9d66d034a794d0e687f4dc7
Dave Strus <dave@getfretless.com>
2016-02-08 16:04:49 -0500

    Create an instance of Mutant in a before block in mutant_spec.

--- spec/mutant_school_api_model/mutant_spec.rb
+++ spec/mutant_school_api_model/mutant_spec.rb
@@ -1,53 +1,38 @@
 require 'test_helper'
 
 describe MutantSchoolAPIModel::Mutant do
+  before do
+    attributes = {
+      mutant_name: 'Wolverine',
+      real_name: 'Logan',
+      power: 'SNIKT',
+      eligibility_begins_at: '1976-06-11',
+      eligibility_ends_at: '2050-05-03',
+      may_advise_beginning_at: '1990-09-25'
+    }
+    @wolverine = Mutant.new attributes
+  end
+
   describe '#save' do
     it 'creates a new mutant' do
-      attributes = {
-        mutant_name: 'Wolverine',
-        real_name: 'Logan',
-        power: 'SNIKT',
-        eligibility_begins_at: '1976-06-11',
-        eligibility_ends_at: '2050-05-03',
-        may_advise_beginning_at: '1990-09-25'
-      }
-      wolverine = Mutant.new attributes
-      actual = Mutant.new(wolverine.save)
-      _(actual.to_h).must_equal(wolverine.to_h)
+      actual = Mutant.new(@wolverine.save)
+      _(actual.to_h).must_equal(@wolverine.to_h)
     end
   end
 
   describe '#find' do
     it 'should retrieve the mutant that was just created' do
-      attributes = {
-        mutant_name: 'Wolverine',
-        real_name: 'Logan',
-        power: 'SNIKT',
-        eligibility_begins_at: '1976-06-11',
-        eligibility_ends_at: '2050-05-03',
-        may_advise_beginning_at: '1990-09-25'
-      }
-      wolverine = Mutant.new attributes
-      wolverine.save
+      @wolverine.save
 
-      actual = Mutant.find(wolverine.id)
-      _(actual.to_h).must_equal(wolverine.to_h)
+      actual = Mutant.find(@wolverine.id)
+      _(actual.to_h).must_equal(@wolverine.to_h)
     end
 
     it 'should return false if we look for a record that was just deleted' do
-      attributes = {
-        mutant_name: 'Wolverine',
-        real_name: 'Logan',
-        power: 'SNIKT',
-        eligibility_begins_at: '1976-06-11',
-        eligibility_ends_at: '2050-05-03',
-        may_advise_beginning_at: '1990-09-25'
-      }
-      wolverine = Mutant.new attributes
-      wolverine.save
-      id = wolverine.id
+      @wolverine.save
+      id = @wolverine.id
 
-      wolverine.destroy
+      @wolverine.destroy
       actual = Mutant.find(id)
       _(actual).must_equal false
       # _(actual).must_be_instance_of(Array)
@@ -57,17 +42,8 @@ describe MutantSchoolAPIModel::Mutant do
 
   describe '#all' do
     it 'should return an array of mutants' do
-      # Create a mutant.
-      attributes = {
-        mutant_name: 'Wolverine',
-        real_name: 'Logan',
-        power: 'SNIKT',
-        eligibility_begins_at: '1976-06-11',
-        eligibility_ends_at: '2050-05-03',
-        may_advise_beginning_at: '1990-09-25'
-      }
-      wolverine = Mutant.new attributes
-      wolverine.save
+      # Create a mutant, so there will be at least one.
+      @wolverine.save
 
       # Make sure `all` returns an Array.
       actual = Mutant.all
