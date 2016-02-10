9c40157865bb0ea60bc9a4d235795bce275e6d7d
Dave Strus <dave@getfretless.com>
2016-02-10 10:03:32 -0500

    Term has many enrollments.

--- lib/mutant_school_api_model/term.rb
+++ lib/mutant_school_api_model/term.rb
@@ -3,6 +3,8 @@ require "mutant_school_api_model/resource"
 module MutantSchoolAPIModel
   class Term < MutantSchoolAPIModel::Resource
 
+    has_many :enrollments
+
     def self.model_specific_attribute_names
       [
         :begins_at,
