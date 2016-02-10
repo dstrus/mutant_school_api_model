15e6b82cb39e8ebd9fd664cece7aa7f489fe3f95
Dave Strus <dave@getfretless.com>
2016-02-10 15:56:52 -0500

    Patch Hash with utility methods.

--- lib/mutant_school_api_model.rb
+++ lib/mutant_school_api_model.rb
@@ -1,6 +1,7 @@
 require 'http'
 require 'json'
 require 'pry'
+require "core_extensions/hash"
 require "mutant_school_api_model/version"
 require "mutant_school_api_model/resource"
 require "mutant_school_api_model/mutant"
