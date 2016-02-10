632223e076b43afa9d5c8593b2654fe8f479841f
Dave Strus <dave@getfretless.com>
2016-02-10 16:47:32 -0500

    Mutant belongs to advisor.

--- lib/mutant_school_api_model/enrollment.rb
+++ lib/mutant_school_api_model/enrollment.rb
@@ -4,5 +4,6 @@ module MutantSchoolAPIModel
   class Enrollment < MutantSchoolAPIModel::Resource
     belongs_to :term
     belongs_to :student, class_name: 'Mutant'
+    attribute_names :student_id, :term_id
   end
 end
--- lib/mutant_school_api_model/mutant.rb
+++ lib/mutant_school_api_model/mutant.rb
@@ -3,12 +3,12 @@ require "mutant_school_api_model/resource"
 module MutantSchoolAPIModel
   class Mutant < MutantSchoolAPIModel::Resource
 
+    belongs_to :advisor, class_name: 'Mutant'
     has_many :enrollments
+    
     attribute_names :mutant_name, :real_name, :power,
       :eligibility_begins_at, :eligibility_ends_at,
       :may_advise_beginning_at
-
-    read_only_attribute_names :advisor
 
   end
 end
