a830b504039eb37c293a4ecb401825cfb933053a
Dave Strus <dave@getfretless.com>
2016-02-21 17:44:07 -0500

    Fix load path in test_helper and add the project root to the load path in console.

--- spec/test_helper.rb
+++ spec/test_helper.rb
@@ -1,4 +1,4 @@
-$LOAD_PATH.unshift File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'mutant_school_api_model'
 require 'minitest/autorun'
 require 'minitest/reporters'
